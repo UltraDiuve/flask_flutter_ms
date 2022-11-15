3436696dafde30fcc0ee5259588a13d3647168c5
PieroMerguez <pit.masse@gmail.com>
2022-11-15 16:03:55 +0100

    Added authentication with firebase

--- frontend/front_flutter/packages/profile_api/test/profile_api_test.dart
+++ frontend/front_flutter/packages/profile_api/test/profile_api_test.dart
@@ -1,6 +1,6 @@
-import 'package:flutter_test/flutter_test.dart';
+// import 'package:flutter_test/flutter_test.dart';
 
-import 'package:profile_api/profile_api.dart';
+// import 'package:profile_api/profile_api.dart';
 
 void main() {
   // test('adds one to input values', () {
--- frontend/front_flutter/packages/profile_repository/test/profile_repository_test.dart
+++ frontend/front_flutter/packages/profile_repository/test/profile_repository_test.dart
@@ -1,6 +1,6 @@
-import 'package:flutter_test/flutter_test.dart';
+// import 'package:flutter_test/flutter_test.dart';
 
-import 'package:profile_repository/profile_repository.dart';
+// import 'package:profile_repository/profile_repository.dart';
 
 void main() {
   // test('adds one to input values', () {
